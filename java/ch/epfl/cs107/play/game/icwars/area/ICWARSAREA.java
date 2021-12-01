2cd4d1d4f353c21524d234a551e816a73b84d76f
ZiZouet <91062845+zizouet@users.noreply.github.com>
2021-12-01 13:16:25 +0100

    Pour de vrai

--- java/ch/epfl/cs107/play/game/icwars/area/ICWARSAREA.java
+++ java/ch/epfl/cs107/play/game/icwars/area/ICWARSAREA.java
@@ -1,15 +1,15 @@
-package ch.epfl.cs107.play.game.tutosSolution.area;
+package ch.epfl.cs107.play.game.icwars.area;
 
 import ch.epfl.cs107.play.game.areagame.Area;
-import ch.epfl.cs107.play.game.tutosSolution.Tuto2;
-import ch.epfl.cs107.play.game.tutosSolution.Tuto2Behavior;
+import ch.epfl.cs107.play.game.icwars.ICWARS;
+import ch.epfl.cs107.play.game.icwars.BEHAVIOR;
 import ch.epfl.cs107.play.io.FileSystem;
 import ch.epfl.cs107.play.math.DiscreteCoordinates;
 import ch.epfl.cs107.play.window.Window;
 
-public abstract class Tuto2Area extends Area {
+public abstract class ICWARSAREA extends Area {
 	
-	private Tuto2Behavior behavior;
+	private BEHAVIOR behavior;
 
     /**
      * Create the area by adding it all actors
@@ -22,7 +22,7 @@ public abstract class Tuto2Area extends Area {
 
     @Override
     public final float getCameraScaleFactor() {
-        return Tuto2.CAMERA_SCALE_FACTOR;
+        return ICWARS.CAMERA_SCALE_FACTOR;
     }
 
     public abstract DiscreteCoordinates getPlayerSpawnPosition();
@@ -33,7 +33,7 @@ public abstract class Tuto2Area extends Area {
     public boolean begin(Window window, FileSystem fileSystem) {
         if (super.begin(window, fileSystem)) {
             // Set the behavior map
-        	behavior = new Tuto2Behavior(window, getTitle());
+        	behavior = new BEHAVIOR(window, getTitle());
             setBehavior(behavior);
             createArea();
             return true;
--- java/ch/epfl/cs107/play/game/icwars/area/MapICwars/Level0.java
+++ java/ch/epfl/cs107/play/game/icwars/area/MapICwars/Level0.java
@@ -1,14 +1,13 @@
-package ch.epfl.cs107.play.game.tutosSolution.area.tuto2;
+package ch.epfl.cs107.play.game.icwars.area.MapICwars;
 
 import ch.epfl.cs107.play.game.areagame.actor.Background;
-import ch.epfl.cs107.play.game.areagame.actor.Foreground;
-import ch.epfl.cs107.play.game.tutosSolution.area.Tuto2Area;
+import ch.epfl.cs107.play.game.icwars.area.ICWARSAREA;
 import ch.epfl.cs107.play.math.DiscreteCoordinates;
 
 /**
  * Specific area
  */
-public class Ferme extends Tuto2Area {
+public class Level0 extends ICWARSAREA {
 	
 	@Override
 	public String getTitle() {
