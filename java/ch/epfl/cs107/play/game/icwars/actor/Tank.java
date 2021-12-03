9aceb885c618aad5715c5a049fb2703d05920131
SidonieBouthors <sidonie.bouthors@epfl.ch>
2021-12-03 16:11:03 +0100

    update

--- java/ch/epfl/cs107/play/game/icwars/actor/Tank.java
+++ java/ch/epfl/cs107/play/game/icwars/actor/Tank.java
@@ -1,14 +1,7 @@
 package ch.epfl.cs107.play.game.icwars.actor;
 
 import ch.epfl.cs107.play.game.areagame.Area;
-import ch.epfl.cs107.play.game.areagame.actor.Interactable;
-import ch.epfl.cs107.play.game.areagame.actor.Interactor;
-import ch.epfl.cs107.play.game.areagame.actor.Orientation;
-import ch.epfl.cs107.play.game.areagame.actor.Sprite;
-import ch.epfl.cs107.play.game.icwars.actor.ICWarsActor;
 import ch.epfl.cs107.play.math.DiscreteCoordinates;
-import ch.epfl.cs107.play.math.Vector;
-import ch.epfl.cs107.play.window.Canvas;
 
 public class Tank extends Unit {
 
